8fbe512fcc16e8666666e282e1495143798d067e
Nathanael Memis <nathanaelmemis@gmail.com>
2024-07-17 14:52:57 +0800

    assets fix

--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -3,16 +3,16 @@ import { Box } from '@mui/material';
 function Technologies({elementId, styles}) {
     return (
         <Box id={elementId} sx={styles}>
-            <img src={'./src/public/home/html.png'} width={'64px'} title='HTML' />
-            <img src={'./src/public/home/css.png'} width={'64px'} title='CSS' />
-            <img src={'./src/public/home/js.png'} width={'64px'} title='Javascript' />
-            <img src={'./src/public/home/react.png'} width={'64px'} title='React' />
-            <img src={'./src/public/home/electron.svg'} width={'64px'} title='Electron' />
-            <img src={'./src/public/home/php.png'} width={'80px'} title='PHP' />
-            <img src={'./src/public/home/nodejs.svg'} width={'120px'} height={'60px'} title='Node.js' />
-            <img src={'./src/public/home/expressjs.svg'} width={'100px'} title='Express JS' />
-            <img src={'./src/public/home/mysql.png'} width={'85px'} title='MySQL' />
-            <img src={'./src/public/home/firebase.svg'} width={'64px'} title='Firebase' />
+            <img src={'./home/html.png'} width={'64px'} title='HTML' />
+            <img src={'./home/css.png'} width={'64px'} title='CSS' />
+            <img src={'./home/js.png'} width={'64px'} title='Javascript' />
+            <img src={'./home/react.png'} width={'64px'} title='React' />
+            <img src={'./home/electron.svg'} width={'64px'} title='Electron' />
+            <img src={'./home/php.png'} width={'80px'} title='PHP' />
+            <img src={'./home/nodejs.svg'} width={'120px'} height={'60px'} title='Node.js' />
+            <img src={'./home/expressjs.svg'} width={'100px'} title='Express JS' />
+            <img src={'./home/mysql.png'} width={'85px'} title='MySQL' />
+            <img src={'./home/firebase.svg'} width={'64px'} title='Firebase' />
         </Box>
     )
 }

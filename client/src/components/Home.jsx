a1f08c523d9db10f81f6cafa869143cfc7bade60
Nathanael Memis <nathanaelmemis@gmail.com>
2024-07-25 15:30:00 +0800

    added dynamic technologies

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,21 +1,19 @@
 import { Box, Container, Typography } from '@mui/material';
-import './home.scss'
 
 import Technologies from './Technologies';
 
 function Home() {
-  function handleTechnologiesMouseOver() {
-    const technologiesAElement = document.getElementById('technologiesA');
-    const technologiesBElement = document.getElementById('technologiesB');
-    technologiesAElement.style.animationPlayState = 'paused';
-    technologiesBElement.style.animationPlayState = 'paused';
-  }
-
-  function handleTechnologiesMouseOut() {
-    const technologiesAElement = document.getElementById('technologiesA');
-    const technologiesBElement = document.getElementById('technologiesB');
-    technologiesAElement.style.animationPlayState = 'running';
-    technologiesBElement.style.animationPlayState = 'running';
+  const sampleTechnologies = {
+    "HTML": './home/html.png', 
+    "CSS": './home/css.png',
+    "Javascript": './home/js.png',
+    "React": './home/react.png',
+    "Electron": './home/electron.svg',
+    "PHP": './home/php.png',
+    "Node.js": './home/nodejs.svg',
+    "Express JS": './home/expressjs.svg',
+    "MySQL": './home/mysql.png',
+    "Firebase": './home/firebase.svg',
   }
 
   return (
@@ -32,92 +30,15 @@ function Home() {
         <Typography>I am a programming enthusiast that loves to create projects I'm interested in.</Typography>
         <Typography>I graduated from the Polytechnic University of the Philippines as a Bachelor in Computer Science.</Typography>
         <Typography>I specialize in Software Development, Web Development, and Machine Learning.</Typography>
-        <Box
-          onMouseOver={handleTechnologiesMouseOver}
-          onMouseOut={handleTechnologiesMouseOut}
+        <Technologies 
+          technologies={sampleTechnologies}
+          height={'64px'}
+          fadeWidth={'100px'}
           sx={{
-            pt: '1em',
-            width: '80%',
-            height: '90px',
-            overflow: 'hidden',
-            position: 'relative',
-          }}>
-          <Box sx={{
-            width: '15%',
-            height: '100%',
-            backgroundImage: 'linear-gradient(to left, rgba(255, 255, 255,0), rgba(255, 255, 255,1))',
-            position: 'absolute',
-            zIndex: '1'
-          }} />
-          <Box sx={{
-            width: '15%',
-            height: '100%',
-            marginLeft: '85%',
-            backgroundImage: 'linear-gradient(to right, rgba(255, 255, 255,0), rgba(255, 255, 255,1))',
-            position: 'absolute',
-            zIndex: '1'
-          }} />
-          <Technologies elementId='technologiesA' styles={{
-            position: 'absolute',
-            width: '68em',
-            '& img': {
-              mr: '2em'
-            },
-            left: '0px',
-            display: 'flex',
-            alignItems: 'center',
-            animation: 'scroll-left-1 20s linear infinite',
-            '@keyframes scroll-left-1': {
-              '0%': {
-                left: '0px'
-              },
-              '49%': {
-                top: '1em'
-              },
-              '50%': {
-                left: '-1088px',
-                top: '100px'
-              },
-              '51%': {
-                left: '1088px',
-                top: '100px'
-              },
-              '52%': {
-                top: '1em'
-              },
-              '100%': {
-                left: '0px'
-              }
-            }
-          }} />
-          <Technologies elementId='technologiesB' styles={{
-            position: 'absolute',
-            width: '68em',
-            '& img': {
-              mr: '2em'
-            },
-            display: 'flex',
-            alignItems: 'center',
-            left: '1088px',
-            animation: 'scroll-left-2 20s linear infinite',
-            '@keyframes scroll-left-2': {
-              '0%': {
-                left: '1088px',
-                top: '100px'
-              },
-              '1%': {
-                top: '1em'
-              },
-              '99%': {
-                top: '1em'
-              },
-              '100%': {
-                left: '-1088px',
-                top: '100px'
-              },
-            }
-          }} />
-        </Box>
+            mt: '2em',
+            width: '92%',
+          }}
+        />
       </Box>
       <Box 
         sx={{
--- client/src/components/Technologies.jsx
+++ client/src/components/Technologies.jsx
@@ -1,18 +1,89 @@
 import { Box } from '@mui/material';
+import { useEffect } from 'react';
 
-function Technologies({elementId, styles}) {
+function Technologies({ technologies, height, sx, fadeWidth, mouseInteraction = true }) {
+    function handleTechnologiesMouseOver() {
+        const technologiesElement = document.querySelectorAll('#technology')
+        technologiesElement.forEach((element) => {
+            element.style.animationPlayState = 'paused'
+        })
+    }
+
+    function handleTechnologiesMouseOut() {
+        const technologiesElement = document.querySelectorAll('#technology')
+        technologiesElement.forEach((element) => {
+            element.style.animationPlayState = 'running'
+        })
+    }
+
+    useEffect(() => {
+        const styleSheet = document.createElement('style');
+        styleSheet.type = 'text/css';
+
+        styleSheet.innerHTML += `
+            @keyframes scrollLeft {
+                0% {
+                    left: 100%;
+                }
+                to {
+                    left: calc(100% - ${Object.keys(technologies).length * parseInt(height) * 1.25}px);
+                }
+            }
+        `
+
+        document.head.appendChild(styleSheet);
+    })
+
+    const FadeBox = (direction) => {
+        return (
+            <Box
+                width={fadeWidth}
+                height={height}
+                sx={{
+                    left: direction === 'left' ? 0 : '',
+                    right: direction === 'right' ? 0 : '',
+                    zIndex: 1,
+                    position: 'absolute',
+                    background: `linear-gradient(to ${direction}, transparent, rgba(255, 255, 255, 1))`
+                }} />
+        )
+    }
+
     return (
-        <Box id={elementId} sx={styles}>
-            <img src={'./home/html.png'} width={'64px'} title='HTML' />
-            <img src={'./home/css.png'} width={'64px'} title='CSS' />
-            <img src={'./home/js.png'} width={'64px'} title='Javascript' />
-            <img src={'./home/react.png'} width={'64px'} title='React' />
-            <img src={'./home/electron.svg'} width={'64px'} title='Electron' />
-            <img src={'./home/php.png'} width={'80px'} title='PHP' />
-            <img src={'./home/nodejs.svg'} width={'120px'} height={'60px'} title='Node.js' />
-            <img src={'./home/expressjs.svg'} width={'100px'} title='Express JS' />
-            <img src={'./home/mysql.png'} width={'85px'} title='MySQL' />
-            <img src={'./home/firebase.svg'} width={'64px'} title='Firebase' />
+        <Box
+            height={height}
+            onMouseOver={mouseInteraction ? handleTechnologiesMouseOver : () => {}}
+            onMouseOut={mouseInteraction ? handleTechnologiesMouseOut : () => {}}
+            sx={{
+                display: 'flex',
+                position: 'relative',
+                overflow: 'hidden',
+                ...sx
+            }}>
+            {FadeBox('left')}
+            {
+                Object.entries(technologies).map(([key, value], index) => {
+                    return (
+                        <Box
+                            id={'technology'}
+                            width={height}
+                            height={height}
+                            key={key}
+                            data-index={index}
+                            sx={{
+                                left: `calc(100% - ${Object.keys(technologies).length * parseInt(height) * 1.25}px)`,
+                                position: 'absolute',
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'center',
+                                animation: `scrollLeft ${Object.keys(technologies).length * 1}s infinite ${index * 1 + .25}s linear`,
+                            }}>
+                            <img src={value} width={'64px'} title={key} style={{ objectFit: 'contain' }} />
+                        </Box>
+                    )
+                })
+            }
+            {FadeBox('right')}
         </Box>
     )
 }
